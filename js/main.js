f6bab3a910bba98eeb85713fe01b97967b2e43a2
= <bezaval@gmail.com>
2022-12-22 18:19:50 +0300

    work is done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
+// * adaptive navigation
 const mobileNavButton = document.querySelector(".mobile-nav-button");
 const mobileNavIcon = document.querySelector(".mobile-nav-button__icon");
 const mobileNav = document.querySelector(".mobile-nav");
 
-
 mobileNavButton.addEventListener("click", function () {
   mobileNavIcon.classList.toggle("active");
   mobileNav.classList.toggle("active");
-  document.body.classList.toggle('no-scroll');
+  document.body.classList.toggle("no-scroll");
 });
+
+// * video
+
+const videoBtn = document.querySelector("#video__story-btn");
+const videoBtnIcon = document.querySelector("#video__story-btn-icon");
+const videoOverlay = document.querySelector("#video__story-overlay");
+const videoFile = document.querySelector("#video-story");
+
+videoBtn.addEventListener("click", function () {
+  function toggleOverlay(event) {
+    if (event.type === "mouseleave") {
+      videoOverlay.classList.add("hidden");
+    } else {
+      videoOverlay.classList.remove('hidden');
+    } 
+  }
+
+  if (videoFile.paused) {
+    videoFile.play();
+    videoBtnIcon.src = "../img/story-video/Icon/puse_white.svg";
+
+    videoOverlay.onmouseleave = toggleOverlay;
+    videoOverlay.onmouseenter = toggleOverlay;
+  } else { 
+    videoFile.pause();
+    videoBtnIcon.src = "../img/story-video/Icon/large/play-white.svg";
+    videoOverlay.onmouseleave = null;
+    videoOverlay.onmouseenter = null;
+  }
+}); 
